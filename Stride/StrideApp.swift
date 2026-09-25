091dcc864d5e54e272e03d854800017aaebc17af
Alexandre de Paula Dias Junior <its_alejunior1997@icloud.com>
2026-09-24 21:23:39 -0300

    Initial Commit

---
+++ Stride/StrideApp.swift
@@ -0,0 +1,17 @@
+//
+//  StrideApp.swift
+//  Stride
+//
+//  Created by Alexandre Junior on 24/09/26.
+//
+
+import SwiftUI
+
+@main
+struct StrideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
